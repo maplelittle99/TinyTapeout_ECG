7ea32d329bd72d909f4f17dc74d4283214a4cff2
Yujin Zheng <59091577+maplelittle99@users.noreply.github.com>
2026-03-04 16:37:08 +0000

    Change back to fit original template

--- src/project.v
+++ src/project.v
@@ -3,18 +3,30 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+`default_nettype none
 module tt_um_dec4to16 (
-    input  wire        rst_n,
-	input  wire        clk,
-	input  wire [3:0]  ui,
-	output reg  [15:0] dec_out,
+	input  wire [2:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
 );
+
+  // All output pins must be assigned. If not used, assign to 0.
+  assign uio_out = 0;
+  assign uio_oe  = 0;
+
+  // List all unused inputs to prevent warnings
+  wire _unused = &{ena, 1'b0};
 
 	always @(posedge clk) begin
     if (!rst_n)
-        dec_out = 16'b0;
+        uo_out = 8'b0;
     else
-        dec_out = 16'b1 << ui;
+        uo_out = 8'b1 << ui_in;
 end
 
 endmodule
