7b8ebd1449f5e1d5a4f9811333adf35e620804d9
Yujin Zheng <59091577+maplelittle99@users.noreply.github.com>
2026-03-04 15:58:50 +0000

    Use ui, uo, uio for names

--- src/project.v
+++ src/project.v
@@ -9,9 +9,11 @@ use IEEE.std_logic_1164.all;
 
 -- The 4 to 16 decoder
 entity tt_um_dec4to16 is
-port( rst_n: in std_logic;
-	addr: in std_logic_vector(0 to 3);
-	dec_out: out std_logic_vector(0 to 15)
+port( 
+	rst_n: in std_logic;	// not reset
+	ui_in: in std_logic_vector(0 to 3);
+	uo_out: out std_logic_vector(0 to 7);
+	uio_out: out std_logic_vector(0 to 7)
 );
 end dec4to16;
 
@@ -19,29 +21,30 @@ architecture rtl of dec4to16 is
 
 begin
 
-process (rst_n, addr)
+	process (rst_n, ui_in)
 	begin
 		if  (rst_n = '0') then
-			dec_out <= "0000000000000000";
+			uo_out <= "00000000";
+			uio_out <= "00000000";
 		else
-			case addr is
-				when "0000" => dec_out <= "0000000000000001";
-				when "0001" => dec_out <= "0000000000000010";
-				when "0010" => dec_out <= "0000000000000100";
-				when "0011" => dec_out <= "0000000000001000";
-				when "0100" => dec_out <= "0000000000010000";
-				when "0101" => dec_out <= "0000000000100000";
-				when "0110" => dec_out <= "0000000001000000";
-				when "0111" => dec_out <= "0000000010000000";
-				when "1000" => dec_out <= "0000000100000000";
-				when "1001" => dec_out <= "0000001000000000";
-				when "1010" => dec_out <= "0000010000000000";
-				when "1011" => dec_out <= "0000100000000000";
-				when "1100" => dec_out <= "0001000000000000";
-				when "1101" => dec_out <= "0010000000000000";
-				when "1110" => dec_out <= "0100000000000000";
-				when "1111" => dec_out <= "1000000000000000";
-				when others => dec_out <= "0000000000000000";
+			case ui_in is
+				when "0000" => uo_out <= "00000001";
+				when "0001" => uo_out <= "00000010";
+				when "0010" => uo_out <= "00000100";
+				when "0011" => uo_out <= "00001000";
+				when "0100" => uo_out <= "00010000";
+				when "0101" => uo_out <= "00100000";
+				when "0110" => uo_out <= "01000000";
+				when "0111" => uo_out <= "10000000";
+				when "1000" => uio_out <= "00000001";
+				when "1001" => uio_out <= "00000010";
+				when "1010" => uio_out <= "00000100";
+				when "1011" => uio_out <= "00001000";
+				when "1100" => uio_out <= "00010000";
+				when "1101" => uio_out <= "00100000";
+				when "1110" => uio_out <= "01000000";
+				when "1111" => uio_out <= "10000000";
+				when others => uio_out <= "00000000";
 			end case;
 		end if;
 end process;
