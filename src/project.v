ab426e2bf469d4a9881d1d464a857a7ace7eb3bb
Yujin Zheng <59091577+maplelittle99@users.noreply.github.com>
2026-03-04 15:48:09 +0000

    Refactor 4 to 16 decoder and update copyright
    Refactor 4 to 16 decoder implementation and update copyright.

--- src/project.v
+++ src/project.v
@@ -1,27 +1,50 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Yujin
  * SPDX-License-Identifier: Apache-2.0
  */
 
-`default_nettype none
+-- 4 to 16 decoder design
+library IEEE;
+use IEEE.std_logic_1164.all;
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+-- The 4 to 16 decoder
+entity tt_um_dec4to16 is
+port( rst_n: in std_logic;
+	addr: in std_logic_vector(0 to 3);
+	dec_out: out std_logic_vector(0 to 15)
 );
+end dec4to16;
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+architecture rtl of dec4to16 is
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+begin
 
-endmodule
+process (rst_n, addr)
+	begin
+		if  (rst_n = '0') then
+			dec_out <= "0000000000000000";
+		else
+			case addr is
+				when "0000" => dec_out <= "0000000000000001";
+				when "0001" => dec_out <= "0000000000000010";
+				when "0010" => dec_out <= "0000000000000100";
+				when "0011" => dec_out <= "0000000000001000";
+				when "0100" => dec_out <= "0000000000010000";
+				when "0101" => dec_out <= "0000000000100000";
+				when "0110" => dec_out <= "0000000001000000";
+				when "0111" => dec_out <= "0000000010000000";
+				when "1000" => dec_out <= "0000000100000000";
+				when "1001" => dec_out <= "0000001000000000";
+				when "1010" => dec_out <= "0000010000000000";
+				when "1011" => dec_out <= "0000100000000000";
+				when "1100" => dec_out <= "0001000000000000";
+				when "1101" => dec_out <= "0010000000000000";
+				when "1110" => dec_out <= "0100000000000000";
+				when "1111" => dec_out <= "1000000000000000";
+				when others => dec_out <= "0000000000000000";
+			end case;
+		end if;
+end process;
+
+end rtl;
+				
