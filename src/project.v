7196ce6880e4e27752a33ddd39ea9abfde7ee54a
Yujin Zheng <59091577+maplelittle99@users.noreply.github.com>
2026-03-04 16:37:51 +0000

    Rename module to tt_um_dec3to8

--- src/project.v
+++ src/project.v
@@ -4,7 +4,8 @@
  */
 
 `default_nettype none
-module tt_um_dec4to16 (
+
+module tt_um_dec3to8 (
 	input  wire [2:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
